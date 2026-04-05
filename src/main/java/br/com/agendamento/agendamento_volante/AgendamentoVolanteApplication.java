ed835bd089e29be642eca7c8596005604436f1a8
FernandesLr <leo1ramosf@gmail.com>
2026-04-05 17:52:01 -0300

    Estrutura inicial do prejeto

---
+++ src/main/java/br/com/agendamento/agendamento_volante/AgendamentoVolanteApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendamento.agendamento_volante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoVolanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoVolanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agendamento/agendamento_volante/AgendamentoVolanteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agendamento.agendamento_volante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoVolanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
