f24a31f7a165d445955e362c6e15b8bd52b2b2ed
FernandesLr <leo1ramosf@gmail.com>
2026-04-19 16:18:25 -0300

    feat: altera notação para restcontroller e adiciona o metodo para listar agendamentos com DTO

--- src/main/java/br/com/agendamento/agendamento_volante/controller/AgendamentoController.java
+++ src/main/java/br/com/agendamento/agendamento_volante/controller/AgendamentoController.java
@@ -1,14 +1,30 @@
 package br.com.agendamento.agendamento_volante.controller;
 
+import br.com.agendamento.agendamento_volante.Dto.AgendamentoDto;
+import br.com.agendamento.agendamento_volante.infrastructure.entity.AgendamentoEntity;
 import br.com.agendamento.agendamento_volante.service.AgendamentoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/agendamentos")
 public class AgendamentoController {
 
     private final AgendamentoService agendamentoService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<AgendamentoDto> listarAgendamentos(){
+        List<AgendamentoEntity> lista = agendamentoService.listarAgendamentos();
+        return lista.stream().map(AgendamentoDto::fromEntity).toList();
+    }
 
 }
