789b4957c0e0d6c09c60e51838c0501d2026ac75
FernandesLr <leo1ramosf@gmail.com>
2026-04-25 15:23:06 -0300

    feat: adiciona método de salvar agendamento no agendamentoservice

--- src/main/java/br/com/agendamento/agendamento_volante/service/AgendamentoService.java
+++ src/main/java/br/com/agendamento/agendamento_volante/service/AgendamentoService.java
@@ -1,10 +1,12 @@
 package br.com.agendamento.agendamento_volante.service;
 
+import br.com.agendamento.agendamento_volante.Dto.AgendamentoDto;
 import br.com.agendamento.agendamento_volante.infrastructure.entity.AgendamentoEntity;
 import br.com.agendamento.agendamento_volante.infrastructure.repository.AgendamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @Service
@@ -21,6 +23,19 @@ public class AgendamentoService {
         }
 
         return agenda;
+    }
+
+    public void salvarAgendamento(AgendamentoDto ag){
+        AgendamentoEntity novaAgenda = AgendamentoEntity.builder()
+                .nomeClinica(ag.nomeClinica())
+                .emailClinica(ag.emailClinica())
+                .dataAgendada(ag.dataAgendada())
+                .endereco(ag.endereco())
+                .tipoServico(ag.tipoServico())
+                .animal(ag.animal())
+                .status("PENDENTE")
+                .build();
+        agendaRepo.save(novaAgenda);
     }
 
 }
