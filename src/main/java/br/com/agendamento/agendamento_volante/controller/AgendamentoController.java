d91c601ca596a8166ad65c6fd22f39bacb73782f
FernandesLr <leo1ramosf@gmail.com>
2026-04-25 15:16:19 -0300

    feat: adiciona método criarAgendamento no controller agendamento com anotação validated

--- src/main/java/br/com/agendamento/agendamento_volante/controller/AgendamentoController.java
+++ src/main/java/br/com/agendamento/agendamento_volante/controller/AgendamentoController.java
@@ -3,19 +3,19 @@ package br.com.agendamento.agendamento_volante.controller;
 import br.com.agendamento.agendamento_volante.Dto.AgendamentoDto;
 import br.com.agendamento.agendamento_volante.infrastructure.entity.AgendamentoEntity;
 import br.com.agendamento.agendamento_volante.service.AgendamentoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/agendamentos")
+@Validated
 public class AgendamentoController {
 
     private final AgendamentoService agendamentoService;
@@ -25,6 +25,12 @@ public class AgendamentoController {
     public List<AgendamentoDto> listarAgendamentos(){
         List<AgendamentoEntity> lista = agendamentoService.listarAgendamentos();
         return lista.stream().map(AgendamentoDto::fromEntity).toList();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarAgendamento(@Valid @RequestBody AgendamentoDto agd){
+        agendamentoService.salvarAgendamento(agd);
     }
 
 }
