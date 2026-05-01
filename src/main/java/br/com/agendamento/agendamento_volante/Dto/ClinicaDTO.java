549ff7d83700fb01a858107fd8543454d13ea5e2
FernandesLr <leo1ramosf@gmail.com>
2026-04-30 22:33:47 -0300

    feat: adiciona validacoes do bean validation no ClinicaDTO

--- src/main/java/br/com/agendamento/agendamento_volante/Dto/ClinicaDTO.java
+++ src/main/java/br/com/agendamento/agendamento_volante/Dto/ClinicaDTO.java
@@ -1,9 +1,27 @@
 package br.com.agendamento.agendamento_volante.Dto;
 
 import br.com.agendamento.agendamento_volante.infrastructure.entity.ClinicaEntity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ClinicaDTO(
-    String nome, String cnpj, String endereco, String telefone, String email, String senha
+    @NotBlank
+    String nome,
+    @NotBlank(message = "O CNPJ é obrigatório")
+    @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter exatamente 14 dígitos numéricos")
+    String cnpj,
+    @NotBlank
+    String endereco,
+    @NotBlank
+    String telefone,
+    @NotBlank
+    @Email(message = "Email deve ser válido")
+    String email,
+    @NotBlank
+    @Size(min = 5, message = "A senha deve conter no mínimo 5 caracteres")
+    String senha
 ) {
 
     public static ClinicaDTO fromEntity(ClinicaEntity clinica){
