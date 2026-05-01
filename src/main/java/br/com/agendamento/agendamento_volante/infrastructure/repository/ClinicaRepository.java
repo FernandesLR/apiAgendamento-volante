e69e24395c7f37decde553c455ca8ccb41c1faf5
FernandesLr <leo1ramosf@gmail.com>
2026-04-30 21:49:08 -0300

    feat: adiciona query method findByEmailAndSenha no ClinicaRepository

--- src/main/java/br/com/agendamento/agendamento_volante/infrastructure/repository/ClinicaRepository.java
+++ src/main/java/br/com/agendamento/agendamento_volante/infrastructure/repository/ClinicaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ClinicaRepository extends JpaRepository<ClinicaEntity, UUID> {
-
+    ClinicaEntity findByEmailAndSenha(String email, String senha);
 }
