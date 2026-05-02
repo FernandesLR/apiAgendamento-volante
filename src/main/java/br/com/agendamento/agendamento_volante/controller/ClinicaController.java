235e8f4c9f69184197bd9064d5e33546a0a83072
FernandesLr <leo1ramosf@gmail.com>
2026-05-02 14:57:57 -0300

    feat: adiciona método de login no controller clinica

--- src/main/java/br/com/agendamento/agendamento_volante/controller/ClinicaController.java
+++ src/main/java/br/com/agendamento/agendamento_volante/controller/ClinicaController.java
@@ -2,10 +2,9 @@ package br.com.agendamento.agendamento_volante.controller;
 
 import br.com.agendamento.agendamento_volante.Dto.ClinicaDTO;
 import br.com.agendamento.agendamento_volante.service.ClinicaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,9 +16,15 @@ public class ClinicaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void criarConta(@Validated @RequestBody ClinicaDTO clinica){
+    public void criarConta(@Valid @RequestBody ClinicaDTO clinica){
         clinicaService.salvar(clinica);
 
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ClinicaDTO login(@Valid @RequestBody ClinicaDTO clinica){
+        return clinicaService.buscarClinica(clinica.email(), clinica.senha());
     }
 
 }
