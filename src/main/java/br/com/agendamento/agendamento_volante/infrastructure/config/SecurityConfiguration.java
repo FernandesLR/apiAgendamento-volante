ea947bf68ad84f05e16d22b5e14d988d59168f25
FernandesLr <leo1ramosf@gmail.com>
2026-05-14 14:23:30 -0300

    feat: implementa classe de configuração do spring security junto com um método de passwordencoder

---
+++ src/main/java/br/com/agendamento/agendamento_volante/infrastructure/config/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package br.com.agendamento.agendamento_volante.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
