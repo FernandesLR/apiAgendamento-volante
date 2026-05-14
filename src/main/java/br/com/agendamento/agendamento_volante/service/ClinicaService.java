395af076b6c56aeebe27f502fb7119db2ef29a41
FernandesLr <leo1ramosf@gmail.com>
2026-05-14 14:16:27 -0300

    feat: implementa a password encoder para encriptar a senha no clinica service

--- src/main/java/br/com/agendamento/agendamento_volante/service/ClinicaService.java
+++ src/main/java/br/com/agendamento/agendamento_volante/service/ClinicaService.java
@@ -1,22 +1,23 @@
 package br.com.agendamento.agendamento_volante.service;
 
 import br.com.agendamento.agendamento_volante.Dto.ClinicaDTO;
-import br.com.agendamento.agendamento_volante.infrastructure.entity.ClinicaEntity;
 import br.com.agendamento.agendamento_volante.infrastructure.repository.ClinicaRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ClinicaService {
     private final ClinicaRepository clinicaRepo;
+    private final PasswordEncoder passwordEncoder;
 
     public ClinicaDTO buscarClinica(String email, String senha) {
         return clinicaRepo.findByEmail(email)
                 .filter(clinica -> passwordEncoder.matches(senha, clinica.getSenha()))
                 .map(ClinicaDTO::fromEntity)
-                .orElseThrow(() -> new EntityNotFoundException("Credenciais inválidas")); // 4. Exceção específica
+                .orElseThrow(() -> new EntityNotFoundException("Credenciais inválidas"));
     }
 
     public void salvar(ClinicaDTO cli){
