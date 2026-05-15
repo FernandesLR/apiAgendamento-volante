1f1ea1289ff1ad360f12a9a2987864c9e64ae8c1
FernandesLr <leo1ramosf@gmail.com>
2026-05-15 14:20:59 -0300

    refactor(security): corrige endpoint de autenticação

--- src/main/java/br/com/agendamento/agendamento_volante/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/br/com/agendamento/agendamento_volante/infrastructure/config/SecurityConfiguration.java
@@ -1,10 +1,13 @@
 package br.com.agendamento.agendamento_volante.infrastructure.config;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -32,8 +35,7 @@ public class SecurityConfiguration {
                             response.setStatus(HttpStatus.FORBIDDEN.value());
                         }))
                 .authorizeHttpRequests( auth -> {
-                    auth.requestMatchers(HttpMethod.POST, "/auth/registrar").permitAll();
-                    auth.requestMatchers(HttpMethod.POST, "/auth/login").permitAll();
+                    auth.requestMatchers(HttpMethod.POST, "/clinicas/auth/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
