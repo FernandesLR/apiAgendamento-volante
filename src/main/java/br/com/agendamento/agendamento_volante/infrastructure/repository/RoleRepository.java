e56a3ee8c7545892c2855e3ff251385f9b4f695c
FernandesLr <leo1ramosf@gmail.com>
2026-05-16 15:08:24 -0300

    refactor(repository): implementa a saída como optional para a query findBynome

--- src/main/java/br/com/agendamento/agendamento_volante/infrastructure/repository/RoleRepository.java
+++ src/main/java/br/com/agendamento/agendamento_volante/infrastructure/repository/RoleRepository.java
@@ -3,6 +3,10 @@ package br.com.agendamento.agendamento_volante.infrastructure.repository;
 import br.com.agendamento.agendamento_volante.infrastructure.entity.RolesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<RolesEntity, Integer> {
+
+    Optional<RolesEntity> findByNome(String nome);
 
 }
