e43613271b686f59816ffe65525da4516f48ef69
FernandesLr <leo1ramosf@gmail.com>
2026-05-17 19:23:34 -0300

    feat(handler): implementa tratamento de erro de usuário já cadastrado e campos inválidos no json

--- src/main/java/br/com/agendamento/agendamento_volante/handle/GlobalExceptionHandler.java
+++ src/main/java/br/com/agendamento/agendamento_volante/handle/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package br.com.agendamento.agendamento_volante.handle;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,17 +13,27 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<Map<String, String>> tratarErroLogin() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("mensagem", "E-mail ou senha inválidos."));
+    // Trata o erro e lança no Service ("Usuário já cadastrado")
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.badRequest()
+                .body(Map.of("erro", ex.getMessage()));
     }
 
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Map<String, String>> tratarErroNegocio(RuntimeException ex) {
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleBadCredentials() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("erro", "Credenciais inválidas"));
+    }
+
+    // Trata o erro de digitação no json
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        String mensagem = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        String campo = ex.getBindingResult().getFieldErrors().get(0).getField();
+
         return ResponseEntity.badRequest()
-                .body(Map.of("mensagem", ex.getMessage()));
+                .body(Map.of("erro", "Erro no campo " + campo + ": " + mensagem));
     }
 }
