3e990b03746b82d0f03e3788d8489f74c8ea6976
FernandesLr <leo1ramosf@gmail.com>
2026-05-28 15:19:44 -0300

    refactor(service): passa a lógica de serviço de autenticação da clinica para o authservice

--- src/main/java/br/com/agendamento/agendamento_volante/service/ClinicaService.java
+++ src/main/java/br/com/agendamento/agendamento_volante/service/ClinicaService.java
@@ -1,8 +1,6 @@
 package br.com.agendamento.agendamento_volante.service;
 
-import br.com.agendamento.agendamento_volante.Dto.ClinicaDTO;
 import br.com.agendamento.agendamento_volante.infrastructure.repository.ClinicaRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,20 +11,5 @@ public class ClinicaService {
     private final ClinicaRepository clinicaRepo;
     private final PasswordEncoder passwordEncoder;
 
-    public ClinicaDTO buscarClinica(String email, String senha) {
-        return clinicaRepo.findByEmail(email)
-                .filter(clinica -> passwordEncoder.matches(senha, clinica.getSenha()))
-                .map(ClinicaDTO::fromEntity)
-                .orElseThrow(() -> new EntityNotFoundException("Credenciais inválidas"));
-    }
-
-    public void salvar(ClinicaDTO cli){
-        var clinica = cli.toEntity();
-
-        String senhaCriptografada = passwordEncoder.encode(clinica.getSenha());
-        clinica.setSenha(senhaCriptografada);
-
-        clinicaRepo.save(clinica);
-    }
 
 }
