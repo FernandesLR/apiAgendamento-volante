3399b9022903e36eba1e6e9319f43c05655ed6fe
FernandesLr <leo1ramosf@gmail.com>
2026-05-28 17:15:24 -0300

    feat(controller): implementa endpoint de excluir usuario

--- src/main/java/br/com/agendamento/agendamento_volante/controller/ClinicaController.java
+++ src/main/java/br/com/agendamento/agendamento_volante/controller/ClinicaController.java
@@ -1,8 +1,14 @@
 package br.com.agendamento.agendamento_volante.controller;
 
+import br.com.agendamento.agendamento_volante.Dto.ClinicaDTO;
 import br.com.agendamento.agendamento_volante.service.ClinicaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/clinicas")
@@ -10,6 +16,14 @@ import org.springframework.web.bind.annotation.*;
 public class ClinicaController {
 
     private final ClinicaService clinicaService;
+
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.OK)
+    public void excluirConta(Authentication auth){
+        String emailLogado = auth.getName();
+
+        clinicaService.excluirConta(emailLogado);
+    }
 
 
 
