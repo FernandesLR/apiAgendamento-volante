da0e9973c3bb9b630fb12fe826e8a02da00af344
FernandesLr <leo1ramosf@gmail.com>
2026-05-28 17:14:03 -0300

    feat(service): implementa método de deletar clinica

--- src/main/java/br/com/agendamento/agendamento_volante/service/ClinicaService.java
+++ src/main/java/br/com/agendamento/agendamento_volante/service/ClinicaService.java
@@ -1,15 +1,28 @@
 package br.com.agendamento.agendamento_volante.service;
 
+import br.com.agendamento.agendamento_volante.Dto.ClinicaDTO;
 import br.com.agendamento.agendamento_volante.infrastructure.repository.ClinicaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ClinicaService {
     private final ClinicaRepository clinicaRepo;
     private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void excluirConta(String email){
+        if(clinicaRepo.findByEmail(email).isEmpty()){
+            throw new EntityNotFoundException("Não é possivel deletar usuário");
+        }
+        clinicaRepo.deleteByEmail(email);
+    }
 
 
 }
