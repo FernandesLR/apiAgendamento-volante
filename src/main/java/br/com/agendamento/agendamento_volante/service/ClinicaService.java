0acd37755a7ec9596363ac1106f31fde7495d97d
FernandesLr <leo1ramosf@gmail.com>
2026-05-31 17:19:02 -0300

    feat(service): implementa classe que altera os dados cadastrais da clinica

--- src/main/java/br/com/agendamento/agendamento_volante/service/ClinicaService.java
+++ src/main/java/br/com/agendamento/agendamento_volante/service/ClinicaService.java
@@ -1,6 +1,8 @@
 package br.com.agendamento.agendamento_volante.service;
 
 import br.com.agendamento.agendamento_volante.Dto.ClinicaDTO;
+import br.com.agendamento.agendamento_volante.Dto.ClinicaUpdateDTO;
+import br.com.agendamento.agendamento_volante.infrastructure.entity.ClinicaEntity;
 import br.com.agendamento.agendamento_volante.infrastructure.repository.ClinicaRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
@@ -22,6 +24,20 @@ public class ClinicaService {
             throw new EntityNotFoundException("Não é possivel deletar usuário");
         }
         clinicaRepo.deleteByEmail(email);
+    }
+    @Transactional
+    public void alterarDados(String emailLogado, ClinicaUpdateDTO dto){
+        ClinicaEntity clinica = clinicaRepo.findByEmail(emailLogado).orElseThrow(() -> new EntityNotFoundException("Usuário não cadastrado"));
+        // email, senha telefone
+        if(dto.email() != null){
+            clinica.setEmail(dto.email());
+        }if(dto.senha() != null){
+            String senhaCriptografada = passwordEncoder.encode(dto.senha());
+            clinica.setSenha(senhaCriptografada);
+        }if(dto.telefone() != null){
+            clinica.setTelefone(dto.telefone());
+        }
+
     }
 
 
