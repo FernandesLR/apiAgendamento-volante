f810e47d7d3a19930c578204476f9e19dfc2f0df
FernandesLr <leo1ramosf@gmail.com>
2026-06-04 17:35:57 -0300

    feat(service): implementa classe de salvar fazendo ligação entre clinica e agendamento

--- src/main/java/br/com/agendamento/agendamento_volante/service/AgendamentoService.java
+++ src/main/java/br/com/agendamento/agendamento_volante/service/AgendamentoService.java
@@ -2,8 +2,12 @@ package br.com.agendamento.agendamento_volante.service;
 
 import br.com.agendamento.agendamento_volante.Dto.AgendamentoDto;
 import br.com.agendamento.agendamento_volante.infrastructure.entity.AgendamentoEntity;
+import br.com.agendamento.agendamento_volante.infrastructure.entity.ClinicaEntity;
 import br.com.agendamento.agendamento_volante.infrastructure.repository.AgendamentoRepository;
+import br.com.agendamento.agendamento_volante.infrastructure.repository.ClinicaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.parser.Entity;
@@ -14,6 +18,7 @@ import java.util.List;
 public class AgendamentoService {
 
     private final AgendamentoRepository agendaRepo;
+    private final ClinicaRepository clinicaRepository;
 
     public List<AgendamentoEntity> listarAgendamentos(){
         List<AgendamentoEntity> agenda = agendaRepo.findAll();
@@ -25,8 +30,21 @@ public class AgendamentoService {
         return agenda;
     }
 
-    public void salvarAgendamento(AgendamentoDto ag){
+    public void salvarAgendamento(AgendamentoDto ag, String usuarioLogado){
+        ClinicaEntity clinicaLogada = clinicaRepository.findByEmail(usuarioLogado)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não cadastrado"));
+
+        // converte os dados que vem do controller para entity
         AgendamentoEntity novaAgenda = ag.toEntity();
+
+
+        novaAgenda.setClinica(clinicaLogada); // Faz a ligação entre a clinica e o agendamento
+
+        novaAgenda.setNomeClinica(clinicaLogada.getNome());
+        novaAgenda.setEmailClinica(clinicaLogada.getEmail());
+        novaAgenda.setEndereco(clinicaLogada.getEndereco());
+        novaAgenda.setStatus("PENDENTE"); // todo agendamento nasce como pendente
+
         agendaRepo.save(novaAgenda);
     }
 
