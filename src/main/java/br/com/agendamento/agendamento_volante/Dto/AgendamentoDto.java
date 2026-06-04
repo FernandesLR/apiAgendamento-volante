9c5d96fc39635ce606d28fd012ff316756a28a84
FernandesLr <leo1ramosf@gmail.com>
2026-06-04 17:17:12 -0300

    refactor(dto): modela o dto do agendamento para receber entregar apenas dados essenciais

--- src/main/java/br/com/agendamento/agendamento_volante/Dto/AgendamentoDto.java
+++ src/main/java/br/com/agendamento/agendamento_volante/Dto/AgendamentoDto.java
@@ -4,50 +4,31 @@ import br.com.agendamento.agendamento_volante.infrastructure.entity.AgendamentoE
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+
 
 public record AgendamentoDto(
-        UUID idAgenda,
         @NotBlank
-        String nomeClinica,
-        @NotBlank
-        String emailClinica,
-
         LocalDateTime dataAgendada,
-        @NotBlank
-        String endereco,
         @NotBlank
         String tipoServico,
         @NotBlank
-        String animal,
-        @NotBlank
-        String status
+        String animal
 ) {
 
     public static AgendamentoDto fromEntity(AgendamentoEntity entity) {
         return new AgendamentoDto(
-                entity.getIdAgenda(),
-                entity.getNomeClinica(),
-                entity.getEmailClinica(),
                 entity.getDataAgendada(),
-                entity.getEndereco(),
                 entity.getTipoServico(),
-                entity.getAnimal(),
-                entity.getStatus()
+                entity.getAnimal()
         );
     }
 
 
     public AgendamentoEntity toEntity(){
         AgendamentoEntity entity = new AgendamentoEntity();
-        entity.setIdAgenda(this.idAgenda);
-        entity.setNomeClinica(this.nomeClinica);
-        entity.setEmailClinica(this.emailClinica);
         entity.setDataAgendada(this.dataAgendada);
-        entity.setEndereco(this.endereco);
         entity.setTipoServico(this.tipoServico);
         entity.setAnimal(this.animal);
-        entity.setStatus(this.status);
         return entity;
     }
 }
