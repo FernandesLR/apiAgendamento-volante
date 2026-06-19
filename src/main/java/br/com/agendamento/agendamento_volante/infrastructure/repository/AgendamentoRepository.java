b5353a0dfecd58834f435cffc6889858a209773e
FernandesLr <leo1ramosf@gmail.com>
2026-06-18 22:04:33 -0300

    feat(repository): implementa query que verifica se existe uma data e hora especifica

--- src/main/java/br/com/agendamento/agendamento_volante/infrastructure/repository/AgendamentoRepository.java
+++ src/main/java/br/com/agendamento/agendamento_volante/infrastructure/repository/AgendamentoRepository.java
@@ -4,9 +4,15 @@ import br.com.agendamento.agendamento_volante.infrastructure.entity.AgendamentoE
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AgendamentoRepository extends JpaRepository<AgendamentoEntity, UUID> {
+
+    List<AgendamentoEntity> findByEmailClinica(String emailLogado);
+
+    boolean existsByDataAgendada(LocalDateTime dataAgendada);
 
 }
