5e03a9ebea3c79c415c8fcaad9bc7a149d0c91a6
FernandesLr <leo1ramosf@gmail.com>
2026-08-09 17:49:04 -0300

    feat(dto): implementa notações de validação para cadastro de admin

--- src/main/java/br/com/agendamento/agendamento_volante/Dto/CadastroAdminDto.java
+++ src/main/java/br/com/agendamento/agendamento_volante/Dto/CadastroAdminDto.java
@@ -1,8 +1,20 @@
 package br.com.agendamento.agendamento_volante.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CadastroAdminDto(
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Insira um email válido!")
+        String email,
+        @NotBlank(message = "Senha é obrigatória")
+        @Size(min = 8)
         String senha,
+        @NotBlank(message = "Telefone é obrigatório")
         String telefone
 ) {
+
 }
