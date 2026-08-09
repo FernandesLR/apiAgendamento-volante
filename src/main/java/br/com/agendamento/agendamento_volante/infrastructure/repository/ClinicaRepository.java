24277ad9b84deb5caac20197d81909879e4728e0
FernandesLr <leo1ramosf@gmail.com>
2026-08-09 18:07:37 -0300

    refactor(repository): renomeação de classe clinicaentity para usuarioentity

--- src/main/java/br/com/agendamento/agendamento_volante/infrastructure/repository/ClinicaRepository.java
+++ src/main/java/br/com/agendamento/agendamento_volante/infrastructure/repository/ClinicaRepository.java
@@ -1,14 +1,14 @@
 package br.com.agendamento.agendamento_volante.infrastructure.repository;
 
-import br.com.agendamento.agendamento_volante.infrastructure.entity.ClinicaEntity;
+import br.com.agendamento.agendamento_volante.infrastructure.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ClinicaRepository extends JpaRepository<ClinicaEntity, UUID> {
-    Optional<ClinicaEntity> findByEmail(String email);
-    Optional<ClinicaEntity> findByEmailOrCnpj(String email, String cnpj);
+public interface ClinicaRepository extends JpaRepository<UsuarioEntity, UUID> {
+    Optional<UsuarioEntity> findByEmail(String email);
+    Optional<UsuarioEntity> findByEmailOrCnpj(String email, String cnpj);
     void deleteByEmail(String email);
 
 }
