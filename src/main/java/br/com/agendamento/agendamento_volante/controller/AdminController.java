75c5997ba5974e378a89182eea57a050e0275bcd
FernandesLr <leo1ramosf@gmail.com>
2026-08-09 17:50:37 -0300

    feat(controller): implementar controller do admin com método de registro

---
+++ src/main/java/br/com/agendamento/agendamento_volante/controller/AdminController.java
@@ -0,0 +1,24 @@
+package br.com.agendamento.agendamento_volante.controller;
+
+import br.com.agendamento.agendamento_volante.Dto.CadastroAdminDto;
+import br.com.agendamento.agendamento_volante.service.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/clinica/auth/admin")
+public class AdminController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarAdmin(@RequestBody @Valid CadastroAdminDto dto) throws Exception {
+        authenticationService.registrarAdmin(dto);
+    }
+
+
+}
